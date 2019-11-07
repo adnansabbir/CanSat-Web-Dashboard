09d97342c3299cacd7c877215b81b00ea107a71b
Adnan Sabbir <adnan.sabbir2012@gmail.com>
2019-11-08 05:59:52 +0600

    Routing changed to change navbar name

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -5,7 +5,7 @@ import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.compon
 export const AppRoutes: Routes = [
   {
     path: '',
-    redirectTo: 'dashboard',
+    redirectTo: 'cansat-tracking',
     pathMatch: 'full',
   }, {
     path: '',
@@ -17,6 +17,6 @@ export const AppRoutes: Routes = [
   }]},
   {
     path: '**',
-    redirectTo: 'dashboard'
+    redirectTo: 'cansat-tracking'
   }
 ]
--- src/app/layouts/admin-layout/admin-layout.routing.ts
+++ src/app/layouts/admin-layout/admin-layout.routing.ts
@@ -3,5 +3,5 @@ import {Routes} from '@angular/router';
 import {DashboardComponent} from '../../pages/dashboard/dashboard.component';
 
 export const AdminLayoutRoutes: Routes = [
-  {path: 'dashboard', component: DashboardComponent},
+  {path: 'cansat-tracking', component: DashboardComponent},
 ];
