9e1b1fb4b90dde9cc07be73a4032f151f9dd2aec
Adnan Sabbir <adnan.sabbir2012@gmail.com>
2019-11-08 05:37:24 +0600

    Deleted unnessary components and module

--- src/app/layouts/admin-layout/admin-layout.module.ts
+++ src/app/layouts/admin-layout/admin-layout.module.ts
@@ -6,13 +6,6 @@ import {FormsModule} from '@angular/forms';
 import {AdminLayoutRoutes} from './admin-layout.routing';
 
 import {DashboardComponent} from '../../pages/dashboard/dashboard.component';
-import {UserComponent} from '../../pages/user/user.component';
-import {TableComponent} from '../../pages/table/table.component';
-import {TypographyComponent} from '../../pages/typography/typography.component';
-import {IconsComponent} from '../../pages/icons/icons.component';
-import {MapsComponent} from '../../pages/maps/maps.component';
-import {NotificationsComponent} from '../../pages/notifications/notifications.component';
-import {UpgradeComponent} from '../../pages/upgrade/upgrade.component';
 
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
@@ -24,14 +17,7 @@ import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
     NgbModule
   ],
   declarations: [
-    DashboardComponent,
-    UserComponent,
-    TableComponent,
-    UpgradeComponent,
-    TypographyComponent,
-    IconsComponent,
-    MapsComponent,
-    NotificationsComponent,
+    DashboardComponent
   ]
 })
 
