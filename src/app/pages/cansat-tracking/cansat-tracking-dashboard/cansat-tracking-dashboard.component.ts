1589e012889a6c5668bece45484c3d0223a60587
Adnan Sabbir <adnan.sabbir2012@gmail.com>
2019-11-10 01:05:13 +0600

    Added data connection between chrome app that reads serial data

--- src/app/pages/cansat-tracking/cansat-tracking-dashboard/cansat-tracking-dashboard.component.ts
+++ src/app/pages/cansat-tracking/cansat-tracking-dashboard/cansat-tracking-dashboard.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {CansatTrackingCards} from '../cansat-tracking.models';
 
 @Component({
@@ -6,7 +6,12 @@ import {CansatTrackingCards} from '../cansat-tracking.models';
   templateUrl: './cansat-tracking-dashboard.component.html',
   styleUrls: ['./cansat-tracking-dashboard.component.scss']
 })
-export class CansatTrackingDashboardComponent implements OnInit {
+export class CansatTrackingDashboardComponent implements OnInit, OnDestroy {
+  browserIsChrome = false;
+  browserConnected = false;
+  browserConnection = null;
+  availablePorts = [];
+
   cards: CansatTrackingCards[] = [
     {title: 'Mission Time', subtitle: '2 min, 25 sec', icon_class_name: 'nc-watch-time'},
     {title: 'GPS Time', subtitle: '', icon_class_name: 'nc-compass-05'},
@@ -19,7 +24,40 @@ export class CansatTrackingDashboardComponent implements OnInit {
   constructor() {
   }
 
+  ngOnDestroy(): void {
+    if (this.browserConnection) {
+      this.browserConnection.disconnect();
+    }
+  }
+
   ngOnInit() {
+    // chrome.runtime.sendMessage('kcjgihiihhdcefbgljdpelhjbbbdjolc', {message: 'Connecting via single msg'}, () => {
+    //   console.log('Message Sent');
+    // });
+    this.connectToSerialReader();
+  }
+
+  connectToSerialReader() {
+    this.browserIsChrome = !!window['chrome'] && (!!window['chrome']['webstore'] || !!window['chrome']['runtime']);
+    if (this.browserIsChrome) {
+      console.log('Connecting to Chrome App');
+      this.browserConnection = window['chrome']['runtime'].connect('kcjgihiihhdcefbgljdpelhjbbbdjolc');
+      this.browserConnection.postMessage({sender: 'cansat-tracker'});
+      this.browserConnection.onMessage.addListener((msg) => {
+        console.log(msg);
+        if (msg['establishConnection'] === true) {
+          this.browserConnected = true;
+        } else if (this.browserConnected && msg['device_list']) {
+          this.availablePorts = msg['device_list'];
+          this.browserConnection.postMessage({
+            port_config: {
+              port: 'COM5',
+              baudRate: 9600
+            }
+          })
+        }
+      });
+    }
   }
 
 }
