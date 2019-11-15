8fb33b5af7a8a56ad5313649ca015e92ba8b6b5c
Adnan Sabbir <adnan.sabbir2012@gmail.com>
2019-11-15 22:20:37 +0600

    Added New Route

--- src/app/pages/cansat-tracking/cansat-tracking.routes.ts
+++ src/app/pages/cansat-tracking/cansat-tracking.routes.ts
@@ -1,9 +1,11 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {CansatTrackingDashboardComponent} from './cansat-tracking-dashboard/cansat-tracking-dashboard.component';
+import {CansatTrackingSettingsComponent} from './cansat-tracking-settings/cansat-tracking-settings.component';
 
 const routes: Routes = [
-  {path: '', component: CansatTrackingDashboardComponent}
+  {path: '', component: CansatTrackingDashboardComponent},
+  {path: 'settings', component: CansatTrackingSettingsComponent},
 ];
 
 @NgModule({
