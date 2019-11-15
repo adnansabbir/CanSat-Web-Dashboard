6b7ab81433c51d1ba7d7e14921ce113bbecd086e
Adnan Sabbir <adnan.sabbir2012@gmail.com>
2019-11-15 21:26:07 +0600

    Added Cesium Config

--- src/app/pages/cansat-tracking/services/cansat-tracking.service.ts
+++ src/app/pages/cansat-tracking/services/cansat-tracking.service.ts
@@ -1,9 +1,18 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {CesiumMapConfig} from '../cansat-tracking.models';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CansatTrackingService {
 
-  constructor() { }
+  mapOptions: CesiumMapConfig = {
+    access_token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.' +
+      'eyJqdGkiOiIwZTkwMTE0NC0xNDI5LTQ0MzEtOGQzMy01NjA3NTA2NzlkMzIiLCJpZCI6MTgxNDQsInNjb3BlcyI6WyJhc3IiLCJnYyJdLCJpYXQiOjE1NzM0MDA3MDl9.' +
+      'gdX9hOVF7dEjO8N2i04QPVthTmfpvdnSk9BvUgU5BCs',
+    model_url: '../../../../../assets/3d_models/CesiumBalloon.glb'
+  };
+
+  constructor() {
+  }
 }
