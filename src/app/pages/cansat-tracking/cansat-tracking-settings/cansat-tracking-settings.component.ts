3f6ecd3aafc7cb1c1d27684c61f1ca2d7b819d6d
Adnan Sabbir <adnan.sabbir2012@gmail.com>
2019-11-15 22:21:23 +0600

    Added Settings component for canset tracking

---
+++ src/app/pages/cansat-tracking/cansat-tracking-settings/cansat-tracking-settings.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cansat-tracking-settings',
+  templateUrl: './cansat-tracking-settings.component.html',
+  styleUrls: ['./cansat-tracking-settings.component.scss']
+})
+export class CansatTrackingSettingsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
