ca09b35559475d04431543be19a93e3393bcaac6
Adnan Sabbir <adnan.sabbir2012@gmail.com>
2019-11-16 00:48:56 +0600

    Added saved Data route

--- src/app/pages/cansat-tracking/cansat-tracking.routes.ts
+++ src/app/pages/cansat-tracking/cansat-tracking.routes.ts
@@ -2,10 +2,12 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {CansatTrackingDashboardComponent} from './cansat-tracking-dashboard/cansat-tracking-dashboard.component';
 import {CansatTrackingSettingsComponent} from './cansat-tracking-settings/cansat-tracking-settings.component';
+import {CansatTrackingSavedDataComponent} from './cansat-tracking-saved-data/cansat-tracking-saved-data.component';
 
 const routes: Routes = [
   {path: '', component: CansatTrackingDashboardComponent},
   {path: 'settings', component: CansatTrackingSettingsComponent},
+  {path: 'saved-data', component: CansatTrackingSavedDataComponent},
 ];
 
 @NgModule({
