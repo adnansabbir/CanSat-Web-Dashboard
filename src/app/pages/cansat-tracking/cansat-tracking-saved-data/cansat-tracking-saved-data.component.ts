2856eca7fd29d0f077c678546f35cd5ba30d6a06
Adnan Sabbir <adnan.sabbir2012@gmail.com>
2019-11-17 02:49:12 +0600

    Storing and downloading data in CSV done

--- src/app/pages/cansat-tracking/cansat-tracking-saved-data/cansat-tracking-saved-data.component.ts
+++ src/app/pages/cansat-tracking/cansat-tracking-saved-data/cansat-tracking-saved-data.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {CansatTrackingService} from '../services/cansat-tracking.service';
-import {CanSatDataSet} from '../cansat-tracking.models';
+import {CanSatDataSet, GpsDataModel, GyroDataModel} from '../cansat-tracking.models';
 import {JsonToCsvService} from '../../../common-services/services/json-to-csv.service';
 
 @Component({
@@ -10,6 +10,15 @@ import {JsonToCsvService} from '../../../common-services/services/json-to-csv.se
 })
 export class CansatTrackingSavedDataComponent implements OnInit {
   canSatData: CanSatDataSet[] = [];
+  csvHeaders = {
+    altitude: 'Altitude',
+    battery_voltage: 'Battery Voltage',
+    gps: 'GPS Data',
+    gyro: 'Gyro Data',
+    mission_time: 'Mission Time',
+    pressure: 'Air Pressure',
+    temp: 'Temperature'
+  };
 
   constructor(private cansatTrackingService: CansatTrackingService,
               private jsonToCsvService: JsonToCsvService) {
@@ -17,7 +26,13 @@ export class CansatTrackingSavedDataComponent implements OnInit {
 
   ngOnInit() {
     this.canSatData = this.cansatTrackingService.getAllCanSatData();
-    console.log(this.jsonToCsvService.exportCSVFile(null, this.canSatData, 'Export'));
+    // this.jsonToCsvService.exportCSVFile(this.csvHeaders, this.canSatData[0].data, this.canSatData[0].name);
+    // console.log(this.jsonToCsvService.convertNestedObjectArrayToFlatArrayObject(this.canSatData[0].data));
   }
 
+  downloadCsv(_id) {
+    const data = this.canSatData.find(obj => obj._id === _id);
+    const unNestedData = data ? this.jsonToCsvService.convertNestedObjectArrayToFlatArrayObject(data.data) : null;
+    this.jsonToCsvService.exportCSVFile(this.csvHeaders, unNestedData, data.name)
+  }
 }
